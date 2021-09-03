db6d19a77b4b1f6eecb8d240f8f1bfe45d9dc832
Edward Stoner <edwardlstoner@gmail.com>
2021-09-03 08:39:16 -0600

    Add type definition for svg components

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,3 +2,9 @@ declare module "*.vue" {
   import Vue from "vue";
   export default Vue;
 }
+
+declare module "*.svg?inline" {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const content: any;
+  export default content;
+}
